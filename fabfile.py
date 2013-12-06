4d916f209ff9f8f3ad0295e4f245f1bbf97c2486
Markus Hubig <mhubig@gmail.com>
2013-12-06 18:57:27 +0100

    Renames, some.
    Signed-off-by: Markus Hubig <mhubig@gmail.com>

--- fabfile.py
+++ fabfile.py
@@ -3,9 +3,9 @@ from fabric.api import local
 def update_slave():
     local('buildslave upgrade-slave slave')
 
-def start_buildslave():
+def start_buildbot():
     local('buildslave start --nodaemon slave')
 
 def run():
     update_slave()
-    start_buildslave()
+    start_buildbot()
